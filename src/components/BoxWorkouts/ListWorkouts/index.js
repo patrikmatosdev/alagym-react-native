ccec426b65085cef3c18fa930a74f76c0a88ebf7
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-04-24 15:54:59 -0300

    End MyWorkouts

--- src/components/BoxWorkouts/ListWorkouts/index.js
+++ src/components/BoxWorkouts/ListWorkouts/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {View} from 'react-native';
 import styles from './styles';
 import {List} from 'react-native-paper';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const ListWorkouts = props => {
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container}>
       <List.Item
         title={props.title}
         description={props.description}
@@ -21,7 +22,7 @@ const ListWorkouts = props => {
           <List.Icon {...props} icon="chevron-right" color="#aaaa" size={35} />
         )}
       />
-    </View>
+    </TouchableOpacity>
   );
 };
 
