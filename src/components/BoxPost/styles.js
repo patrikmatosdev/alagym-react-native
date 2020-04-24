193e34af06b594a0c134238e6395fb884aad4fed
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-04-24 10:14:55 -0300

    Update css

--- src/components/BoxPost/styles.js
+++ src/components/BoxPost/styles.js
@@ -3,7 +3,7 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   cointainer: {
     flex: 1,
-    paddingVertical: 20,
+    paddingTop: 20,
     paddingHorizontal: 15,
     marginTop: 10,
     backgroundColor: '#ffff',
--- src/components/SocialButtons/styles.js
+++ src/components/SocialButtons/styles.js
@@ -2,22 +2,21 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 20,
-  },
-
-  containerSocial: {
-    flexDirection: 'row',
-    borderBottomColor: '#ddd',
+    marginTop: 20,
+    borderBottomColor: '#ffff',
     borderTopColor: '#ddd',
     borderLeftColor: '#ffff',
     borderRightColor: '#ffff',
     borderWidth: 1,
   },
 
+  containerSocial: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+  },
+
   socialButtons: {
     flex: 1,
-    marginTop: 10,
-    paddingVertical: 10,
   },
 });
 
