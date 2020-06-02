52549cb4a5bb26913a82f6181bbccf78cc892796
Patrik <patrikmatos.tecinfo@gmail.com>
2020-06-02 14:27:13 -0300

    Editing style in add scrollview

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import styles from './styles';
 import BoxChat from './BoxChat';
 import Avat from '../../assets/img/patrik.jpg';
@@ -7,14 +7,16 @@ import Avat from '../../assets/img/patrik.jpg';
 const Chat = () => {
   return (
     <View style={styles.cointainer}>
-      <BoxChat
-        name="Patrik"
-        surname="Matos"
-        menssage="Minha primeira Mensagem!"
-        avatar={Avat}
-        hour="13:00"
-        notification="5"
-      />
+      <ScrollView>
+        <BoxChat
+          name="Patrik"
+          surname="Matos"
+          menssage="Minha primeira Mensagem!"
+          avatar={Avat}
+          hour="13:00"
+          notification="5"
+        />
+      </ScrollView>
     </View>
   );
 };
--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -3,16 +3,19 @@ import {View, Text} from 'react-native';
 import styles from './styles';
 import BoxContact from './BoxContact/index';
 import Avat from '../../assets/img/patrik.jpg';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export const Contacts = () => {
   return (
     <View style={styles.container}>
-      <BoxContact
-        avatar={Avat}
-        name="Patrik"
-        surname="Matos"
-        status="Uma frase como status."
-      />
+      <ScrollView>
+        <BoxContact
+          avatar={Avat}
+          name="Patrik"
+          surname="Matos"
+          status="Uma frase como status."
+        />
+      </ScrollView>
     </View>
   );
 };
