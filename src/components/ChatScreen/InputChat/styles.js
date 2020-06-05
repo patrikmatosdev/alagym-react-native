aa3c7d5c045d8808090b1a680adae750314505e9
Patrik <patrikmatos.tecinfo@gmail.com>
2020-06-05 16:07:22 -0300

    Remove padding

--- src/components/ChatScreen/InputChat/styles.js
+++ src/components/ChatScreen/InputChat/styles.js
@@ -4,7 +4,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderRadius: 15,
-    padding: 10,
     overflow: 'hidden',
     backgroundColor: '#ffffff',
     flexDirection: 'row',
